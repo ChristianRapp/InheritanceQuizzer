8440b20b2085d0fbc9f16ca8b9c385e79200fcbf
ChristianRapp <christian.rapp@hotmail.com>
2016-01-14 08:43:52 -0700

    nice

--- Inheritance Quizzer/src/Main.java
+++ Inheritance Quizzer/src/Main.java
@@ -6,21 +6,60 @@ public class Main
 		
 		public static void main(String[] args)
 			{
-				A a = new B(4);
-				System.out.println(a.getNum());
+				B b = new B();
+				System.out.println(b.getANum());
 			}
 		
 		public void addQuestions()
 		{
 			
-			questions.add(new Quiz("A a = new B(); What will return if a.getANum() is called?",1,false,"5","10","17","15"));
+			questions.add(new Quiz("A a = new B(); What will return if a.getANum() is called?",1,"5","Error","17","15"));
 			
-			questions.add(new Quiz("A a = new B(4); What will the value of num be?",2, false, "40","20","25", "30"));
+			questions.add(new Quiz("A a = new B(4); What will the value of num be?",2, "40","20","Error", "30"));
 			
-			questions.add(new Quiz("B b = new A(); What will return is b.getANum()",3, false, "17", "15" , "Error", "10"));
+			questions.add(new Quiz("B b = new A(); What will return is b.getANum()",3, "Error", "15" , "17", "10"));
 			
-			questions.add(new Quiz(" ", 4, false, "", "", "", ""));
+			questions.add(new Quiz("B b = new A(3); What will the value of num be?", 4,  "Error", "14", "15", "30"));
 			
+			questions.add(new Quiz("B b = new B(); What will b.getANum() return?", 5,  "17", "10", "5", "15"));
+			
+			questions.add(new Quiz("", 6,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 7,  "", "", "", ""));
+
+			questions.add(new Quiz("", 8,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 9,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 10,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 11,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 12,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 13,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 14,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 15,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 16,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 17,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 18,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 19,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 20,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 21,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 22,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 23,  "", "", "", ""));
+			
+			questions.add(new Quiz("", 24,  "", "", "", ""));
 		}
 		
 
--- Inheritance Quizzer/src/Quiz.java
+++ Inheritance Quizzer/src/Quiz.java
@@ -4,19 +4,18 @@ import java.util.*;
 		{
 		private String question;
 		private int questionNumber;
-		private boolean trueOrFalse;
-		private boolean isTOrF;
+		//private boolean trueOrFalse;
+		//private boolean isTOrF;
 		private String correctAnswer;
 		private String answer2;
 		private String answer3;
 		private String answer4;
 		
 		
-		public Quiz(String q,int qn, boolean tOrF, String c1, String a2, String a3, String a4)
+		public Quiz(String q,int qn, String c1, String a2, String a3, String a4)
 			{
 			question = q;
 			questionNumber = qn;
-			trueOrFalse = tOrF;
 			correctAnswer = c1;
 			answer2 = a2;
 			answer3 = a3;
