29cdec05d4438ac2add952e317ed7d1a76afc1c3
Christian Rapp <christian.rapp@hotmail.com>
2016-01-19 20:19:26 -0700

    Added some questions

--- Inheritance Quizzer/src/Main.java
+++ Inheritance Quizzer/src/Main.java
@@ -1,16 +1,99 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+
+
 public class Main
 	{
 
-		static ArrayList questions = new ArrayList<Quiz>();
+	static String name = "";
+	static int score =0;
+	
+	
+		static ArrayList<Quiz> questions = new ArrayList<Quiz>();
 		
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
-				B b = new B();
-				System.out.println(b.getANum());
+			A a = new A();
+			System.out.println(a.changeStr());
+			
+			   addQuestions();
+			   mainMenu();
+			   askQuestions();
+				//B b = new B();
+				//System.out.println(b.getANum());
 			}
 		
-		public void addQuestions()
+		public static void askQuestions()
+		{
+			Collections.shuffle(questions);
+			
+			for(Quiz fred: questions)
+			{
+				System.out.println(fred.getQuestion());
+			}
+			
+		}
+		
+		public static void mainMenu() throws IOException
+		{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Would you like to have you name(1) or remain anonymous(2)");
+			switch(userInput.nextInt())
+			{
+			case 1:
+				{
+				System.out.println("What is your name?");
+				name = userInput.nextLine();
+				}
+			case 2:	
+				{
+				name = "anonymous";
+				}
+			}
+			
+			System.out.println("Would you like to take the quiz(1) or check the high scores(2)?");
+			switch(userInput.nextInt())
+			{
+			case 1:
+				{
+				System.out.println("What is your name?");
+				}
+			case 2:	
+				{
+				printScores();
+				}
+			}
+		}
+		
+		public static void writeScores() throws FileNotFoundException, UnsupportedEncodingException
+		{
+			
+			PrintWriter writer = new PrintWriter("Scores.txt", "UTF-8");
+			writer.println(name + ":" + score);
+			writer.close();
+			
+		}
+		
+		public static void printScores() throws IOException
+		{
+			BufferedReader file = new BufferedReader(new FileReader("Scores.txt"));  
+
+			String line = null;
+			
+			while((line = file.readLine()) != null)
+			{
+				System.out.println(line);
+			} 	
+		}
+		
+		public static void addQuestions()
 		{
 			
 			questions.add(new Quiz("A a = new B(); What will return if a.getANum() is called?",1,"5","Error","17","15"));
@@ -21,17 +104,18 @@ public class Main
 			
 			questions.add(new Quiz("B b = new A(3); What will the value of num be?", 4,  "Error", "14", "15", "30"));
 			
-			questions.add(new Quiz("B b = new B(); What will b.getANum() return?", 5,  "17", "10", "5", "15"));
+			questions.add(new Quiz("B b = new A(); What will b.changeStr return?", 5,  "Error", "STR2", "STR1", "STR"));
 			
-			questions.add(new Quiz("", 6,  "", "", "", ""));
-			
-			questions.add(new Quiz("", 7,  "", "", "", ""));
+			questions.add(new Quiz("B b = new B(); What will b.getANum() return?", 6,  "17", "10", "5", "15"));
 
-			questions.add(new Quiz("", 8,  "", "", "", ""));
 			
-			questions.add(new Quiz("", 9,  "", "", "", ""));
+			questions.add(new Quiz("B b = new B(); What will b.changeStr() return?", 7,  "STR2", "STR1", "Error", "STR"));
+
+			questions.add(new Quiz("B b = new B(2); What will b.getNumber() return?", 8,  "15", "20", "10", "5"));
 			
-			questions.add(new Quiz("", 10,  "", "", "", ""));
+			questions.add(new Quiz("A a = new A(); What will a.getANum() return?", 9,  "5", "10", "15", "1"));
+			
+			questions.add(new Quiz("A a = new A(): What will a.changeStr() return?", 10,  "STR1", "STR2", "Error", "STR"));
 			
 			questions.add(new Quiz("", 11,  "", "", "", ""));
 			
--- Inheritance Quizzer/src/Quiz.java
+++ Inheritance Quizzer/src/Quiz.java
@@ -21,6 +21,66 @@ import java.util.*;
 			answer3 = a3;
 			answer4 = a4;
 			}
+
+
+		public String getQuestion() {
+			return question;
+		}
+
+
+		public void setQuestion(String question) {
+			this.question = question;
+		}
+
+
+		public int getQuestionNumber() {
+			return questionNumber;
+		}
+
+
+		public void setQuestionNumber(int questionNumber) {
+			this.questionNumber = questionNumber;
+		}
+
+
+		public String getCorrectAnswer() {
+			return correctAnswer;
+		}
+
+
+		public void setCorrectAnswer(String correctAnswer) {
+			this.correctAnswer = correctAnswer;
+		}
+
+
+		public String getAnswer2() {
+			return answer2;
+		}
+
+
+		public void setAnswer2(String answer2) {
+			this.answer2 = answer2;
+		}
+
+
+		public String getAnswer3() {
+			return answer3;
+		}
+
+
+		public void setAnswer3(String answer3) {
+			this.answer3 = answer3;
+		}
+
+
+		public String getAnswer4() {
+			return answer4;
+		}
+
+
+		public void setAnswer4(String answer4) {
+			this.answer4 = answer4;
+		}
 		
 		
 		
